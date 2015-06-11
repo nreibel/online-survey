8e77fc46454dd7b8c30a7d3eb3fa9fb5a3e0703f
nir1si <external.Nicolas.Reibel@de.bosch.com>
2015-06-11 14:27:18 +0200

    Added IP and date to results

--- submit.php
+++ submit.php
@@ -2,6 +2,9 @@
 
   $myfile = fopen("results.csv", "a+") or die("Can't open file");
 
+  fwrite($myfile,  $_SERVER['REMOTE_ADDR'] . ';');
+  fwrite($myfile,  date('H:i:s d-m-y') . ';');
+  
   foreach ($_POST as $key => $value) {
     fwrite($myfile,  $value . ';');
   }
